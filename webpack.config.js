27f27616bac8d02a508c0399caff5e8a2db9d4ca
Aleksey <alexey-step@mail.ru>
2021-07-02 23:02:51 +0500

    chore (add basic app)

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendor",
+          test: /node_modules/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+  }
+
+  if (isProd) {
+    config.minimize = true,
+    config.minimizer = [
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+  optimization: optimization(),
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    open: false,
+    port: 8000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: ['file-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      inject: 'body'
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  devtool: 'source-map'
+}
