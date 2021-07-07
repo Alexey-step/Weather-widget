fedb9f29fa090dcf3ffef27509fdd8c342de3398
Aleksey <alexey-step@mail.ru>
2021-07-07 19:33:22 +0500

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import retargetEvents from "react-shadow-dom-retarget-events";
 import App from "./components/app/app";
 import createAPI from "./store/api/api";
 import { reducer } from "./store/reducer";
-
-import "./index.scss";
 
 const api = createAPI();
 
@@ -19,10 +18,22 @@ export const store = configureStore({
       },
     }),
 });
+class WidgetWeather extends HTMLElement {
+  mountPoint: HTMLDivElement;
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#weather-widget")
-);
+  connectedCallback() {
+    this.mountPoint = document.createElement("div");
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(this.mountPoint);
+    retargetEvents(shadowRoot);
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      this.mountPoint
+    );
+  }
+}
+
+customElements.define("weather-widget", WidgetWeather);
